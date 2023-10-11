9e084586b52c78d3867d86249c5952620605c899
TendaniMamadi <mamadi.tendani@gmail.com>
2023-10-11 10:23:05 +0200

    for demo

--- index.js
+++ index.js
@@ -37,36 +37,18 @@ app.use(express.json());
 
 
 // Index route
-app.get('/', (req, res) => {
-    
-    res.render('index')
-});
-
-app.get('/waiters/:username', (req, res) => {
-    // Implement logic to use the name entered in the textbox
-    const username = req.params.username;
-    // Fetch days from the database
-    res.render('waiter_selection',{username: username});
-});
-
-app.post('/waiters/:username', (req, res) => {
-    const selectedDays = req.body.days; // This will be an array of selected days
-    const username = req.params.username;
-    console.log(`${username} selected the following days: ${selectedDays}`);
-    res.redirect('/waiters/',{username: username});
-});
-
-
+app.get('/', routeInstance.homeRoute);
+app.post('/', routeInstance.homeRoute);
+app.get('/waiters/:username', routeInstance.recognizeUser);
+app.post('/waiters/:username',routeInstance.showDays);
 app.get('/days', (req, res) => {
     // Implement logic to show which days waiters are available
     // Fetch data from the database and render a template
     res.render('waiter_availability');
 });
 
-// // Clear database route (POST)
-// app.post('/clear', (req, res) => {
-//     res.redirect('/')
-// });
+// Clear database route (POST)
+app.post('/clear',routeInstance.clearingRoute);
 
 //PORT
 const PORT = process.env.PORT || 2025;
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,57 @@
-export default function routes(frontendInstance,logic){
-    
+export default function routes(frontendInstance, logic) {
+    const homeRoute = async (req, res) => {
+        const enteredUsername = req.body.username;
+        const enteredPassword = req.body.password;
+        // // const correctUsername = enteredUsername;
+        // // const correctPassword = 'password123';
+
+        if (enteredUsername && enteredPassword) {
+            res.redirect('/waiters/' + enteredUsername);
+        }
+        else {
+
+            res.render('index');
+        }
+
+    }
+
+    const recognizeUser = async (req, res) => {
+        // Implement logic to use the name entered in the textbox
+        const username = req.params.username;
+        // Fetch days from the database
+        res.render('waiter_selection', { username: username });
+    }
+
+    const showDays = async (req, res) => {
+        const selectedDays = req.body.days; // This will be an array of selected days
+        const username = req.params.username;
+
+        if (selectedDays) {
+            if (selectedDays.length >= 2) {
+                req.flash('success', 'Days successfully added.');
+                console.log(`${username} selected the following days: ${selectedDays}`);
+            } else {
+                req.flash('warning', 'A minimum of 2 days should be checked!');
+            }
+        } else {
+            req.flash('error', 'Please select days');
+        }
+
+        res.redirect('/waiters/' + username);
+    };
+
+
+
+    const clearingRoute = async (req, res) => {
+        await logic.resetDaysForNewWeek();
+        res.redirect('/')
+    };
+
+
+    return {
+        homeRoute,
+        recognizeUser,
+        showDays,
+        clearingRoute
+    }
 }
